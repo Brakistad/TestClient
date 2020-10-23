33cd38b3bc3a6c5f914e97362da0f85a8a1a02f3
oyvind <brakistad@gmail.com>
2020-10-23 15:56:44 +0200

    getting ready for transferring data to new server

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
 import json
+
+import isoweek
+import maya as maya
 import paho.mqtt.client as mqtt
 import time as t
-from influxdb import DataFrameClient, InfluxDBClient
+from influxdb import InfluxDBClient
 import random as rand
 import numpy as np
 import pandas
-import datetime
+from datetime import datetime, timedelta
+from paho.mqtt.client import Client
 import matplotlib.pyplot as plt
+import csv
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -86,6 +91,7 @@ def gen_data(n, field):
 def transmit_gen_data(client, database, field, data, time):
     milliseconds_in_day = 3600 * 24 * 1000
     topic = "POINT/PUSH"
+    datalist = []
     if field == "temperature":
         sensor_ids = [
             "wheel1_temp1",
@@ -113,7 +119,7 @@ def transmit_gen_data(client, database, field, data, time):
     else:
         sensor_ids = ["sense_imu",
                       "wheel_imu"]
-    time = datetime.datetime.utcnow()
+    time = datetime.utcnow()
     data = {
         "measurement": "train",
         "tags": {
@@ -135,16 +141,16 @@ def transmit_gen_data(client, database, field, data, time):
                 field: y_i
             })
             data["time"] = "%s" % (time.isoformat() + "Z")
-            payload = json.dumps({"database": database, "data": data})
-            client.publish(topic, payload, 2)
-            t.sleep(0.2)
+            datalist.append(data)
+    payload = json.dumps({"database": database, "data": datalist})
+    client.publish(topic, payload, 2)
 
 
 def transmit(client):
-    time = datetime.datetime.utcnow()
+    time = datetime.utcnow()
     database = "testcargo"
     topic = "POINT/PUSH"
-    fields = ["temperature",
+    fields = ["Temperature",
               "gps",
               "accx",
               "accy",
@@ -176,22 +182,287 @@ def push_test_data_to_mqtt():
     t.sleep(4)
 
 
-def connect_influxdb():
-    influx_client = InfluxDBClient('127.0.0.1', port=8086, database='cargo_net', username='cemit',
-                                   password='3rn3DZKreAQK7AJc')
-    query = "SELECT \"Temperature\" FROM \"train\" WHERE time >= 1601006430705ms and time <= 1601014100769ms GROUP BY \"sensor_id\""
+# def get_wheel_data_from_week(week, seconds_per_sample):
+#     """
+#     Gets wheel temperature data within a given week, then calls a group function in order to group the data according to cargonet specs
+#     :param week:
+#     :return:
+#     """
+#     influx_client = InfluxDBClient('127.0.0.1', port=8086, database='cargo_net', username='cemit',
+#                                    password='3rn3DZKreAQK7AJc')
+#     w_stop = isoweek.Week(week.year, week.week + 1)
+#     start = int(t.mktime(week.monday().timetuple()) * 1000)
+#     # start = 1601006430705
+#     stop = int(t.mktime(w_stop.monday().timetuple()) * 1000)
+#     print("Start: %s, Stop: %s" % (start, stop))
+#     # stop = 1601014100769
+#     query = f"SELECT \"Temperature\" FROM \"train\" WHERE time >= {start}ms and time <= {stop}ms GROUP " \
+#             f"BY \"sensor_id\" "
+#     try:
+#         result = influx_client.query(query)
+#     except:
+#         raise Exception("could not send request to database")
+#     series = {}
+#     for i in result.raw['series']:
+#         id = i['tags']['sensor_id']
+#         time, value = map(list, zip(*i['values']))
+#         series.update(
+#             {'%s' % id: {'time': time, 'value': value}})
+#     groups = [("wheel_Ch01", "wheel_Ch06"),
+#               ("wheel_Ch02", "wheel_Ch07"),
+#               ("wheel_Ch03", "wheel_Ch09"),
+#               ("wheel_Ch08",)]
+#     final = group(groups, series, seconds_per_sample)
+#     return pandas.DataFrame(final)
+#
+#
+# def group(groups, series, seconds_per_sample):
+#     final = {}
+#     for group in groups:
+#         length = len(group)
+#         if length == 2:
+#             time, value = mean_for_groups(series[group[0]], series[group[1]], seconds_per_sample)
+#             final.update({f"{group[0] + group[1]}": {"time": time, "value": value}})
+#         else:
+#             final.update({f"{group[0]}": {"time": series[group[0]]['time'], "value": series[group[0]]['value']}})
+#     return final
+#
+#
+# def mean_for_groups(serie1, serie2, period):
+#     time = []
+#     value = []
+#     i2 = 0
+#     mean_n = 0.0
+#     mean_flag = False
+#     new_mean = True
+#     if maya.parse(serie1['time'][0]).datetime() > maya.parse(serie2['time'][0]).datetime():
+#         s1 = serie2
+#         s2 = serie1
+#     else:
+#         s1 = serie1
+#         s2 = serie2
+#     for timestamp1, val in zip(s1['time'], s1['value']):
+#         if is_greater(timestamp1, s2['time'][i2]) and within_range(period, timestamp1, s2['time'][i2]):
+#             time.append(timestamp1)
+#             value.append((val + s2['value'][i2]) / 2.0)
+#             i2 += 1
+#         elif is_greater(timestamp1, s2['time'][i2]):
+#             while is_greater(timestamp1, s2['time'][i2]) and not within_range(period, timestamp1, s2['time'][i2]):
+#                 value.append(s2['value'][i2])
+#                 time.append(s2['time'][i2])
+#                 i2 += 1
+#             time.append(timestamp1)
+#             value.append((val + s2['value'][i2]) / 2.0)
+#             i2 += 1
+#         else:
+#             time.append(timestamp1)
+#             value.append(val)
+#     return time, value
+#
+#
+# def is_greater(t1, t2):
+#     time1 = maya.parse(t1).datetime()
+#     time2 = maya.parse(t2).datetime()
+#     return time2 < time1
+#
+#
+# def within_range(period, t1, t2):
+#     time1 = maya.parse(t1).datetime()
+#     time2 = maya.parse(t2).datetime()
+#     return time2 - timedelta(seconds=period / 2.0) < time1 > time2 + timedelta(seconds=period / 2.0)
+
+
+def connect_influxdb(host='127.0.0.1', db='testcargo', from_time="1d", to_time="now()", field="Temperature"):
+    print("connecting to influxdb")
+    influx_client = InfluxDBClient(host, port=8086, database=db, username='cemit',
+                                   password='3rn3DZKreAQK7AJc', ssl=True)
+
+    print("connected to influxdb")
+    query = f"SELECT \"{field}\" FROM \"train\" WHERE time >= now()-{from_time} AND time <= {to_time} GROUP BY \"sensor_id\""
+    print("sending query")
     result = influx_client.query(query)
-    series = {}
-    for i in result.raw['series']:
-        id = i['tags']['sensor_id']
-        time, value = map(list, zip(*i['values']))
-        series.update(
-            {'%s' % id: {'time': time, 'values': value}})
-
-    print(series)
-    plt.plot(series['wheel_Ch04']['time'], series['wheel_Ch04']['values'])
-    plt.show()
+    print("query sent")
+    return result
 
 
+# '2010-16::AX4-E02-T': line['Ch02 (°C)'],
+# '2010-16::AX4-E03-T': line['Ch03 (°C)'],
+# '2010-16::AX4-E04-T': line['Ch04 (°C)'],
+# '2010-16::AX4-E05-T': line['Ch05 (°C)'],
+# '2010-16::AX4-E06-T': line['Ch06 (°C)'],
+# '2010-16::AX4-E07-T': line['Ch07 (°C)'],
+# '2010-16::AX4-E08-T': line['Ch08 (°C)'],
+# '2010-16::AX4-E09-T': line['Ch09 (°C)']
+def fetch_csv(filename='data'):
+    reader = csv.DictReader(open(filename + '.csv', 'rt'))
+    dict_list = []
+    print('Loading csv ...')
+    for line in reader:
+        dict_list.extend([
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E01-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch01 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E02-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch02 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E03-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch03 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E04-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch04 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E05-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch05 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E06-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch06 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E07-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch07 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E08-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch08 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            },
+            {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': '2010-16::AX4-E09-T'
+                },
+                'fields': {
+                    'Temperature': line['Ch09 (°C)']
+                },
+                'time': datetime.strptime(line['Reading Timestamp (UTC)'], '%m/%d/%Y %I:%M:%S %p').isoformat() + 'Z'
+            }
+        ])
+    print('Finished loading csv')
+    return dict_list
 
-connect_influxdb()
+
+# def parse_influxdb(host, db, msgs):
+#     print('Connecting to influxdb ...')
+#     influx_client = InfluxDBClient(host, port=8086, database=db, username='cemit',
+#                                    password='3rn3DZKreAQK7AJc', ssl=True, verify_ssl=True)
+#     print('Connection successful!')
+#     print('Deleting database ' + db + ' ...')
+#     influx_client.drop_database(db)
+#     print('Deleted database ' + db + '!')
+#     print('Creating database ' + db + ' ...')
+#     influx_client.create_database(db)
+#     print('Created database ' + db + '!')
+#     print('Writing loaded csv series to database ...')
+#     yolo = []
+#     for msg in msgs:
+#         if msg['fields']['Temperature'] == '<SensorError>' or msg['fields']['Temperature'] == '<OutOfRangeHighError>' or \
+#                 msg['fields']['Temperature'] == '<OutOfRangeError>' or msg['fields'][
+#             'Temperature'] == '<OutOfRangeLowError>':
+#             pass
+#         else:
+#             msg['fields']['Temperature'] = float(msg['fields']['Temperature'])
+#             yolo.append(msg)
+#     influx_client.write_points(yolo, time_precision='ms')
+#     print('Finished writing!')
+
+
+def test_traffic_mqtt():
+    client = Client(client_id="test")
+    client.on_connect = on_connect
+    client.username_pw_set(username="cemit", password="SuperPassord!")
+    client.connect(host="52.164.202.250", port=1883)
+    client.loop_start()
+    t.sleep(5)
+
+    t.sleep(2)
+    client.disconnect()
+    t.sleep(2)
+
+
+def sort_influx_result_to_mqtt(result):
+    packet = {'database': 'testcargo', 'data': []}
+    for serie in result['series']:
+        for value in serie['values']:
+            point = {
+                'measurement': 'train',
+                'tags': {
+                    'sensor_id': serie['tags']['sensor_id']
+                },
+                'fields': {
+                    serie['columns'][1]: value[1]
+                },
+                'time': value[0]
+            }
+            packet['data'].append(point)
+    return packet
+def send_to_mqtt(packet, db = "testcargo"):
+    packet['database'] = db
+    client = Client(client_id="test")
+    client.on_connect = on_connect
+    client.username_pw_set(username="cemit", password="SuperPassord!")
+    client.connect(host="52.164.202.250", port=1883)
+    client.loop_start()
+    t.sleep(5)
+    transmit(client)
+    t.sleep(2)
+    client.disconnect()
+    t.sleep(2)
+
+# parse_influxdb('influxdb.devcemi.com')
+# parse_influxdb('influxdb.devcemi.com', 'cargo_net', messages)
+# test_traffic_mqtt()
+result = connect_influxdb("influxdb.devcemi.com", "cargo_net", from_time="14d").raw
+packet = sort_influx_result_to_mqtt(result)
